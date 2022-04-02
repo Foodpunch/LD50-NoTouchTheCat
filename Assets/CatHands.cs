83edefd41fc818d87af05b86a2ddf13a8418dabe
SmallGuillotine <foodpunch@gmail.com>
2022-04-02 15:49:08 +0800

    Uhhh shieettt base game stuff
    Simple cat
    Hand eye tracking
    swiping anims
    slash anims

---
+++ Assets/CatHands.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatHands : MonoBehaviour
+{
+    Vector3 mouseInput;
+    Vector3 mouseDir;
+
+    public GameObject[] SecondaryObject;
+    public enum TRANSFORM
+    {
+        UP,
+        RIGHT,
+        LEFT,
+        DOWN
+    }
+    public TRANSFORM LookDirection = TRANSFORM.UP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetDirectionToMouse();
+        if(SecondaryObject!=null)
+        {
+            for (int i = 0; i < SecondaryObject.Length; ++i)
+            {
+                SecondaryObject[i].transform.rotation = gameObject.transform.rotation;
+            }
+        }
+       
+    }
+
+    void SetDirectionToMouse()
+    {
+        mouseInput = Input.mousePosition;
+        Vector3 mousePosWorld = Camera.main.ScreenToWorldPoint(mouseInput);
+        mousePosWorld.z = 0;
+        mouseDir = mousePosWorld - transform.position;
+        switch(LookDirection)
+        {
+            case TRANSFORM.RIGHT:
+                transform.right = mouseDir;
+                break;
+            case TRANSFORM.UP:
+                transform.up = mouseDir;
+                break;
+            case TRANSFORM.LEFT:
+                transform.right = -mouseDir;
+                break;
+            case TRANSFORM.DOWN:
+                transform.up = -mouseDir;
+                break;
+
+        }
+
+    }
+}
